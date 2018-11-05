e442a467c41ecc9ef7fdfbc026115a13439fa0ac
StaffordC <stafford.carh@gmail.com>
2018-11-05 13:46:32 -0500

    Made a react blank
    React stuff is loaded up now

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import TodoList from './TodoList'
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+      <TodoList addItem={this.addItem}/>
+      </div>
+    );
+  }
+}
+
+export default App;
